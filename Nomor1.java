c3e170cf40d0d0385449678e96310cca4904633e
AhmadNidzom <thecrimsen99@gmail.com>
2024-10-24 13:24:07 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,22 @@
 public class Nomor1{
     public static void main(String[] args){
+            int anak = 0;
+    float tunjangan;
+    
+    Scanner scanner = new Scanner(System.in); 
+
+    System.out.print("Jumlah anggota keluarga :");
+    anak = scanner.nextInt();
+
+    if (anak <= 3){
+        tunjangan = anak * 100000;
+        System.out.println("besar tunjangan kesehatan =" + tunjangan);
+
+    }else if (anak > 3){
+        tunjangan = 300000 + (anak * 50000);
+        System.out.println("besar tunjangan kesehatan =" + tunjangan);
+    }
+    
 
     }
 }
