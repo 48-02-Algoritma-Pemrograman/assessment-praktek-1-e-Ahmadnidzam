5d10e86f6376d1f2581ebe0f22df9d4a6c19d61f
AhmadNidzom <thecrimsen99@gmail.com>
2024-10-24 13:30:19 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,6 +1,6 @@
 public class Nomor1{
     public static void main(String[] args){
-            int anak = 0;
+    int anak = 0;
     float tunjangan;
     
     Scanner scanner = new Scanner(System.in); 
@@ -8,15 +8,16 @@ public class Nomor1{
     System.out.print("Jumlah anggota keluarga :");
     anak = scanner.nextInt();
 
-    if (anak <= 3){
+    if (anak >=1 && anak <= 3){
         tunjangan = anak * 100000;
-        System.out.println("besar tunjangan kesehatan =" + tunjangan);
+        System.out.println("Besar tunjangan kesehatan =" + tunjangan);
 
     }else if (anak > 3){
-        tunjangan = 300000 + (anak * 50000);
-        System.out.println("besar tunjangan kesehatan =" + tunjangan);
+        tunjangan = 300000 + ((anak - 3) * 50000);
+        System.out.println("Besar tunjangan kesehatan =" + tunjangan);
+    }else{
+        return;
     }
     
-
     }
 }
